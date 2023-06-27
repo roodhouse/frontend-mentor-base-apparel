930daa9961c741d65a9599bcd4310a0f12e6711e
roodhouse <rughjm@gmail.com>
2023-06-27 09:05:31 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      gradientOne: '#F8BFBF',
+      gradientTwo: '#EE8B8B',
+      brown: '#423A3A',
+      darkPink: '#CE9898',
+      hotPink: '#EE8B8B',
+      offWhite: '#DBDBDB'
+    }
+  },
+  plugins: [],
+}
